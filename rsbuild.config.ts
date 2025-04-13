7353eba63ffc7cde5dea6b80cd65a43ab071ca8e
Lex Trofimov <daslef93@gmail.com>
2025-04-13 21:10:26 +0300

    add title and meta

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 export default defineConfig({
+  html: {
+    title: "Feedback",
+    meta: {
+      description: "feedback widget"
+    }
+  },
   plugins: [pluginReact(), pluginSvgr()],
 });
